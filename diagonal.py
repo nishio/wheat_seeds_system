2fa50406e6d26340c0804fc4ecbe4276520d3eb6
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-11-26 01:53:01 +0900

    add color variation

--- diagonal.py
+++ diagonal.py
@@ -1,18 +1,32 @@
 import reference
 import pack
+import textures
 
 PACK_NAME = "diagonal"
 DESCRIPTION = "Diagonal Blocks"
-IS_EXPERIMENTAL = True
+IS_EXPERIMENTAL = False
+
+BASES = ["diagonal1", "diagonal2",
+         "diagonal3", "diagonal4", "diagonal5"]
 
 
 def model_refs(models={}):
-    return reference.add(
-        800, models,
-        ["diagonal1", "diagonal2",
-         "diagonal3", "diagonal4", "diagonal5"])
+    sections = zip(range(800, 900, 20), BASES)
+    for offset, name in sections:
+        reference.make_variation(
+            offset, name, textures.WOOL_TEXTURES, models)
+    return models
 
 
 def main():
     models = model_refs({})
-    pack.copy_files("common", models.values(), [])
+
+    for base in BASES:
+        pack.make_variation_modelfiles(
+            base,
+            f"item/{base}",
+            "0", textures.WOOL_TEXTURES
+        )
+
+    pack.copy_files("generated", models.values(), [])
+    pack.copy_files("common", BASES, [])
